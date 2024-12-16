b3f66adc3296f0ba3b486e72e9fbc6ec301ca7c0
FernandoAlves2024 <jfcunhaalves@gmail.com>
2024-12-16 20:49:25 -0300

    Netflix

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let botaosom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botaoInfo = document.querySelector(".link-info")
+let modal = document.querySelector(".modal") 
+
+botaosom.addEventListener("click", ligarSom)
+botaoInfo.addEventListener("click", mostrarModal)
+
+function ligarSom(){
+    video.muted =  !video.muted
+
+}
+function mostrarModal(){
+    
+}
+
